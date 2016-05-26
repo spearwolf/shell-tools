d36beb4502854c9337faa363dd761bae8dfbf2e4
Wolfger Schramm <wolfger@spearwolf.de>
2016-05-26 13:58:35 +0200

    added caddyserver and nvm-upgrade script

---
+++ nvm-upgrade.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+cd ~/.nvm
+
+git fetch --tags
+TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
+echo "Checking out tag $TAG..."
+git checkout "$TAG"
+
+source ~/.nvm/nvm.sh
